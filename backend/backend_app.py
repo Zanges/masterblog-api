a8b97debaa8d1e24e35ae7968d897f7e39e68ce9
Zanges <3868054+Zanges@users.noreply.github.com>
2024-12-10 21:46:59 +0100

    implemented creating

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,41 @@ POSTS = [
 ]
 
 
+def get_free_id():
+    free_id = 1
+    used_ids = [post["id"] for post in POSTS]
+    while free_id in used_ids:
+        free_id += 1
+    return free_id
+
+
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def create_post():
+    data = request.get_json()
+    return_data, status_code = {}, 400 # Default values
+    if "title" not in data:
+        if "error" not in return_data:
+            return_data["error"] = {}
+        return_data["error"]["title"] = "Title is required"
+    if "content" not in data:
+        if "error" not in return_data:
+            return_data["error"] = {}
+        return_data["error"]["content"] = "Content is required"
+
+    if not return_data:
+        new_post = {
+            "id": get_free_id(),
+            "title": data["title"],
+            "content": data["content"],
+        }
+        POSTS.append(new_post)
+        return_data, status_code = new_post, 201
+    return jsonify(return_data), status_code
 
 
 if __name__ == '__main__':
